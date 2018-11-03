6d52e01cc09613a1938045e1b9b7db98a8fc5bda
Varun Ramesh <varunramesh@live.com>
2018-11-03 00:50:06 -0700

    Everything except Org membership.

---
+++ create_db.sql
@@ -0,0 +1,43 @@
+-- ENTITIES
+
+CREATE TABLE USERS (
+    login TEXT,
+    name TEXT,
+    company TEXT,
+    location TEXT,
+    scanned BOOLEAN,
+    PRIMARY KEY (login)
+);
+
+CREATE TABLE ORGS (
+    login TEXT,
+    name TEXT,
+    location TEXT,
+    PRIMARY KEY (login)
+);
+
+CREATE TABLE REPOS (
+    owner TEXT,
+    name TEXT,
+    FOREIGN KEY(owner) REFERENCES USERS(login),
+    PRIMARY KEY (owner, name)
+);
+
+-- RELATIONSHIPS
+
+CREATE TABLE FOLLOW (
+    follower TEXT,
+    followee TEXT,
+    FOREIGN KEY(follower) REFERENCES USERS(login),
+    FOREIGN KEY(followee) REFERENCES USERS(login),
+    PRIMARY KEY (follower, followee)
+);
+
+CREATE TABLE STAR (
+    user TEXT,
+    repo_owner TEXT,
+    repo_name TEXT,
+    FOREIGN KEY(user) REFERENCES USERS(login),
+    FOREIGN KEY(repo_owner, repo_name) REFERENCES REPOS(owner, name),
+    PRIMARY KEY (user, repo_owner, repo_name)
+);
--- scrape.py
+++ scrape.py
@@ -1,32 +1,90 @@
+#!/usr/bin/env python3
+
 import os
 import sqlite3
 
-if __name__ == "__main__":
-    initialize = not os.path.isfile("data.db")
-    conn = sqlite3.connect('data.db')
+from github import Github
+
+
+def insert_named_user(conn, gh_user):
     c = conn.cursor()
+    c.execute(
+        "INSERT OR IGNORE INTO USERS VALUES (?, ?, ?, ?, ?)",
+        (gh_user.login, gh_user.name, gh_user.company, gh_user.location, False),
+    )
+    conn.commit()
+
+
+def insert_follow(conn, follower, followee):
+    c = conn.cursor()
+    c.execute(
+        "INSERT OR IGNORE INTO FOLLOW VALUES (?, ?)", (follower.login, followee.login)
+    )
+    conn.commit()
+
+
+def insert_star(conn, user, repo):
+    c = conn.cursor()
+    c.execute(
+        "INSERT OR IGNORE INTO STAR VALUES (?, ?, ?)",
+        (user.login, repo.owner.login, repo.name),
+    )
+    conn.commit()
+
+
+def insert_repo(conn, repo):
+    c = conn.cursor()
+    c.execute(
+        "INSERT OR IGNORE INTO REPOS VALUES (?, ?)", (repo.owner.login, repo.name)
+    )
+    conn.commit()
+
+
+def mark_as_scanned(conn, gh_user):
+    c = conn.cursor()
+    c.execute("UPDATE USERS SET scanned = TRUE WHERE login = ?", gh_user.login)
+    conn.commit()
+
+
+if __name__ == "__main__":
+    gh = Github(os.environ["GITHUB_OAUTH_TOKEN"])
+
+    initialize = not os.path.isfile("data.db")
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+    c.execute("PRAGMA foreign_keys = ON;")
 
     if initialize:
-        c.execute("""
-            CREATE TABLE USERS (username TEXT PRIMARY_KEY, scanned BOOLEAN);
-        """)
-
-        c.execute("""
-            CREATE TABLE REPO (
-                username TEXT,
-                reponame TEXT,
-                PRIMARY KEY (username, reponame)
-            );
-        """)
-
-        c.execute("""
-            CREATE TABLE FOLLOW (
-                follower TEXT,
-                followee TEXT,
-                FOREIGN KEY(follower) REFERENCES USERS(username),
-                FOREIGN KEY(followee) REFERENCES USERS(username),
-                PRIMARY KEY (follower, followee)
-            );
-        """)
-
+        for command in open("create_db.sql").read().split(";"):
+            c.execute(command)
         conn.commit()
+
+        insert_named_user(conn, gh.get_user("rameshvarun"))
+
+    while True:
+        unscanned = [
+            row[0]
+            for row in c.execute("SELECT login FROM USERS WHERE scanned = 0").fetchall()
+        ]
+
+        for user in unscanned:
+            gh_user = gh.get_user(user)
+
+            for follower in gh_user.get_followers():
+                insert_named_user(conn, follower)
+                insert_follow(conn, follower, gh_user)
+
+            for followee in gh_user.get_following():
+                insert_named_user(conn, followee)
+                insert_follow(conn, gh_user, followee)
+
+            for repo in gh_user.get_repos():
+                insert_repo(conn, repo)
+
+            for repo in gh_user.get_starred():
+                insert_named_user(conn, repo.owner)
+                insert_repo(conn, repo)
+                insert_star(conn, gh_user, repo)
+
+            mark_as_scanned(conn, gh_user)
+            sys.exit()
