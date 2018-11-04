7e6f792a166011da61b0dba6302b84e93e2f0125
Varun Ramesh <varunramesh@live.com>
2018-11-04 01:19:49 -0700

    Community detection - runs out of memory on my machine, will have to try on cluster.

---
+++ scripts/community-detection.py
@@ -0,0 +1,18 @@
+import snap
+
+if __name__ == "__main__":
+    print("Loading graph...")
+    FIn = snap.TFIn("results/snap-follow.graph")
+    graph = snap.TNGraph.Load(FIn)
+
+    ugraph = snap.ConvertGraph(snap.PUNGraph, graph)
+
+    print("Performing community detection...")
+    CmtyV = snap.TCnComV()
+    modularity = snap.CommunityCNM(ugraph, CmtyV)
+    print("Modularity:", modularity)
+
+    with open("results/communities.txt", "w") as file:
+        for Cmty in CmtyV:
+            file.write(repr([NI for NI in Cmty]))
+            file.write("\n")
