ee883202244e3cb01086a0ea7e2cace635e407bf
Varun Ramesh <varunramesh@live.com>
2018-11-07 20:02:09 -0800

    Prune option for snap-follow-graph

--- scripts/snap-follow-graph.py
+++ scripts/snap-follow-graph.py
@@ -1,26 +1,38 @@
 import snap
+import click
+
 from data import followers, users
 
-if __name__ == "__main__":
+@click.command()
+@click.option('--prune', is_flag=True)
+@click.argument('output', type=click.Path())
+def generate_graph(prune, output):
     graph = snap.TNGraph.New()
 
-    print ("Adding users...")
-    for user in users():
-        graph.AddNode(user['id'])
+    # If we aren't pruning edges, then we need the whole graph.
+    if not prune:
+        print ("Adding users...")
+        for user in users():
+            graph.AddNode(user['id'])
 
     print ("Adding follow edges...")
     for follow in followers():
+        # If source or destination don't exist, then we need to create them.
         src, dst = follow['user_id'], follow['follower_id']
         if not graph.IsNode(src):
-            print("Adding missing node:", src)
             graph.AddNode(src)
-
         if not graph.IsNode(dst):
-            print("Adding missing node:", dst)
             graph.AddNode(dst)
-            
+
         graph.AddEdge(src, dst)
 
-    FOut = snap.TFOut("results/snap-follow.graph")
+    print("Nodes:", graph.GetNodes())
+    print("Edges:", graph.GetEdges())
+
+    FOut = snap.TFOut(output)
     graph.Save(FOut)
     FOut.Flush()
+
+
+if __name__ == '__main__':
+    generate_graph()
