1a77a5f5ad1f874529f41aac78cacfb2c778c207
Varun Ramesh <varunramesh@live.com>
2018-12-09 12:20:50 -0800

    PageRank formatting

--- scripts/page-rank.py
+++ scripts/page-rank.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python2
+from __future__ import print_function
+
 import snap
 import click
 
 from data import users
 from utils import get_user_id_to_login
+
 
 @click.command()
 @click.argument('input', type=click.Path())
@@ -20,7 +23,7 @@ def page_rank(input):
 
     scores = sorted([(PRankH[item], item) for item in PRankH], reverse=True)[:100]
     for i, (score, id) in enumerate(scores):
-        print(i + 1, "&", id_to_login[id], "&", "{:.2f}".format(score * 4), "\\\\")
+        print(i + 1, "&", id_to_login[id], "&", score, "\\\\")
 
 if __name__ == "__main__":
     page_rank()
