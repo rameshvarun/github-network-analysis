20443052c007f3f516129f18b0869013a501c42a
Varun Ramesh <varunramesh@live.com>
2018-12-08 16:08:09 -0800

    Company homogeneity

--- scripts/community-detection/homogeneity-analysis.py
+++ scripts/community-detection/homogeneity-analysis.py
@@ -22,17 +22,25 @@ def get_user_to_orgs():
         user_to_orgs[record['user_id']].add(record['org_id'])
     return user_to_orgs
 
+@cached('results/user-to-company.pickle')
+def get_user_to_company():
+    print("Generating user to company mapping...")
+    return { user['id']: user['company'] for user in users() if user['company'] != None }
+
 if __name__ == "__main__":
     user_to_orgs = get_user_to_orgs()
-    # org_id_to_login = get_org_id_to_login()
+    user_to_company = get_user_to_company()
 
     communities = [c for c in load_communities("results/communities.txt") if len(c) >= COMMUNITY_MIN_SIZE]
     print("Number of communities:", len(communities))
 
     org_homogeneities = []
+    company_homogeneities = []
 
     with click.progressbar(communities) as bar:
         for community in bar:
+            company_count = Counter(user_to_company[user] for user in community if user in user_to_company)
+
             org_count = Counter()
             for user in community:
                 org_count.update(user_to_orgs[user])
@@ -42,7 +50,18 @@ if __name__ == "__main__":
                 org_homogeneity = most_common_org[0][1] / len(community)
                 org_homogeneities.append(org_homogeneity)
 
+            most_common_company = company_count.most_common(1)
+            if len(most_common_company) > 0:
+                company_homogeneity = most_common_company[0][1] / len(community)
+                company_homogeneities.append(company_homogeneity)
+
+    plt.subplot(211)
     plt.hist(org_homogeneities, log=True)
     plt.xlabel('Org. Homogeneity')
+    plt.ylabel('Frequency')
+
+    plt.subplot(212)
+    plt.hist(company_homogeneities, log=True)
+    plt.xlabel('Company Homogeneity')
     plt.ylabel('Frequency')
     plt.show()
